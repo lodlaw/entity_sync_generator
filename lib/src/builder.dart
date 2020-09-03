d43d3c20da2b4e50266ebd63c02f4593f7e7fae1
Anh  Pham <anhphamduy@outlook.com>
2020-09-03 16:55:53 +1000

    Complete builder

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -1,4 +1,6 @@
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/dart/element/visitor.dart';
 import 'package:build/build.dart';
 import 'package:entity_sync/entity_sync.dart';
 import 'package:source_gen/source_gen.dart';
@@ -13,6 +15,49 @@ class UseSerializationGenerator
   @override
   generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
-    return '// haha this has worked perfectly123';
+    final visitor = ModelVisitor();
+
+    final baseElement = annotation.read('baseClass').typeValue.element;
+    baseElement.visitChildren(visitor);
+
+    final sourceBuilder = StringBuffer();
+
+    // open class name
+    sourceBuilder.writeln(
+        " class \$_${element.displayName} extends ${baseElement.displayName} with SerializableMixin { ");
+
+    // open toMap method
+    sourceBuilder.writeln("Map toMap() { return {");
+
+    // properties of map
+    for (final propertyName in visitor.fields.keys) {
+      sourceBuilder.writeln("\"$propertyName\": this.$propertyName,");
+    }
+
+    // close toMap method
+    sourceBuilder.writeln("};}");
+
+    // close class name
+    sourceBuilder.writeln("}");
+
+    return sourceBuilder.toString();
+  }
+}
+
+class ModelVisitor extends SimpleElementVisitor {
+  DartType className;
+  Map<String, DartType> fields = Map();
+
+  @override
+  visitConstructorElement(ConstructorElement element) {
+    className = element.type.returnType;
+    return super.visitConstructorElement(element);
+  }
+
+  @override
+  visitFieldElement(FieldElement element) {
+    fields[element.name] = element.type;
+
+    return super.visitFieldElement(element);
   }
 }
