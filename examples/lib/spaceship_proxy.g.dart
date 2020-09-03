6885e9da802d9dbdd0dca076b9b5ea766086adb9
Anh  Pham <anhphamduy@outlook.com>
2020-09-03 12:52:06 +1000

    Readd examples with flutter intergration

---
+++ examples/lib/spaceship_proxy.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'spaceship_proxy.dart';
+
+// **************************************************************************
+// EntitySyncGenerator
+// **************************************************************************
+
+// haha this has worked perfectly
